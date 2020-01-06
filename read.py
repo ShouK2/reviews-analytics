35e122c2cf1e9446078cd808352c10168bd72e4b
ShouK2 <sf991217@gmail.com>
2020-01-06 14:03:29 +0800

    add word count version function

--- read.py
+++ read.py
@@ -1,12 +1,15 @@
 data = []
 count = 0
-with open('reviews.txt', 'r') as f:
+with open('reviews.txt', 'r', encoding = 'utf-8') as f:
     for line in f:
         data.append(line.strip())
         count += 1
         if count % 10 == 0:
             print(len(data))
 print('讀取完了, 總共有', len(data), '筆資料')
+
+print(data[0])
+
 
 sum_len = 0
 for d in data:
@@ -17,11 +20,35 @@ print('留言的平均長度為', sum_len/len(data))
 new = []
 for d in data:
     if len(d) < 59:
-        new.append(d)
+       new.append(d)
 print('一共有', len(new), '比留言長度小於59')
 
-sceon = []
+
+#文字計數
+
+wc = {} # word_count
 for d in data:
-    if 'son' in d:
-        sceon.append(d)
-print('一共有', len(sceon), '筆')
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1 # 新增新的key進wc字典
+for word in wc:
+    if wc[word] > 400:
+        print(word, wc[word])
+
+print(len(wc))
+
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('這個字沒有出現過喔!')
+print('感謝使用本查詢功能')
+
+
